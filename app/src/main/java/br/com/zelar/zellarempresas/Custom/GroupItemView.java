d8ebf3653d051250173440f394939101f6678a01
enzotiezzi <enzo_tiezzi@hotmail.com>
2016-07-13 22:53:08 -0300

    niveis

--- app/src/main/java/br/com/zelar/zellarempresas/Custom/GroupItemView.java
+++ app/src/main/java/br/com/zelar/zellarempresas/Custom/GroupItemView.java
@@ -1,16 +1,30 @@
 package br.com.zelar.zellarempresas.Custom;
 
 import android.content.Context;
+import android.media.Image;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import br.com.zelar.zellarempresas.Empresas.GestaoEmpresaTreeViewModel;
+import br.com.zelar.zellarempresas.Http.HttpClientHelper;
+import br.com.zelar.zellarempresas.Http.ICallback;
 import br.com.zelar.zellarempresas.R;
+import br.com.zelar.zellarempresas.Session.SessionManager;
 import br.com.zelar.zellarempresas.Utilities.ObjectUtilities;
+import br.com.zelar.zellarempresas.Utilities.Utils;
 
 /**
  * Created by enzo on 13/07/2016.
@@ -24,14 +38,19 @@ public class GroupItemView extends LinearLayout
     private Context context;
     private View v;
 
+    private int nivel;
     private GestaoEmpresaTreeViewModel gestaoEmpresaTreeViewModel;
+    private List<GestaoEmpresaTreeViewModel> gestaoEmpresaTreeViewModels;
 
     private ImageView imageViewTipo;
+    private ImageView imageViewOpen;
     private TextView textViewNumeroGrupo;
     private TextView textViewNomeGrupo;
     private TextView textViewLocais;
     private TextView textViewPessoas;
     private TextView textViewVagas;
+
+    private ViewGroup appendGroup;
 
     public GroupItemView(Context context)
     {
@@ -58,18 +77,39 @@ public class GroupItemView extends LinearLayout
         setup();
     }
 
+    public void setNivel(int nivel)
+    {
+        this.nivel = nivel;
+
+        int margin_left = nivel * 24;
+
+        LinearLayout.LayoutParams layoutParams = (LayoutParams) imageViewOpen.getLayoutParams();
+        layoutParams.setMargins(margin_left, 0, 0, 0);
+
+        imageViewOpen.setLayoutParams(layoutParams);
+    }
+
     private void initialize()
     {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         v = inflater.inflate(R.layout.gestao_empresa_item, this);
 
+        gestaoEmpresaTreeViewModels = new ArrayList<>();
+        nivel = 1;
+
         imageViewTipo = (ImageView) v.findViewById(R.id.imageViewTipo);
+        imageViewOpen = (ImageView) v.findViewById(R.id.imageViewOpen);
         textViewNumeroGrupo = (TextView) v.findViewById(R.id.textViewNumeroGrupo);
         textViewNomeGrupo = (TextView) v.findViewById(R.id.textViewNomeGrupo);
         textViewLocais = (TextView) v.findViewById(R.id.textViewLocais);
         textViewPessoas = (TextView) v.findViewById(R.id.textViewPessoas);
         textViewVagas = (TextView) v.findViewById(R.id.textViewVagas);
+
+        appendGroup = (ViewGroup) v.findViewById(R.id.appendGroup);
+
+        imageViewOpen.setOnClickListener(imageViewOpen_click);
+        textViewNomeGrupo.setOnClickListener(imageViewOpen_click);
     }
 
     private void setup()
@@ -88,12 +128,101 @@ public class GroupItemView extends LinearLayout
         {
             case LOCAL:
                 imageViewTipo.setImageResource(R.drawable.localizacao_top);
+                imageViewOpen.setVisibility(INVISIBLE);
+                textViewNomeGrupo.setOnClickListener(null);
                 break;
             case GRUPO:
                 imageViewTipo.setVisibility(GONE);
                 textViewNumeroGrupo.setVisibility(VISIBLE);
                 textViewNumeroGrupo.setText("G" + ObjectUtilities.getValue(gestaoEmpresaTreeViewModel.getNumero()));
                 break;
+        }
+    }
+
+    OnClickListener imageViewOpen_click = new OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+            if(gestaoEmpresaTreeViewModels.size() == 0)
+            {
+                nivel++;
+
+                String idNivel = gestaoEmpresaTreeViewModel.getId();
+                String idUsuario = new SessionManager(context).getPreferences("idUsuario");
+
+                String url = Utils.buildURL(context, "Mobile/BuscarNiveisAbaixo?idNivel="+idNivel+"&idUsuario="+idUsuario);
+
+                HttpClientHelper.sendRequest(context, "get", url, new ICallback()
+                {
+                    @Override
+                    public void onRequestEnd(int statusCode, Throwable t, String response)
+                    {
+                        if(statusCode == 200 && t == null)
+                        {
+                            GestaoEmpresaTreeViewModel[] empresas = new Gson().fromJson(response, GestaoEmpresaTreeViewModel[].class);
+
+                            gestaoEmpresaTreeViewModels.addAll(Arrays.asList(empresas));
+
+                            if(empresas.length > 0)
+                            {
+                                for(int i = 0; i < empresas.length; i++)
+                                {
+                                    GroupItemView groupItemView = new GroupItemView(context);
+                                    groupItemView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                                    groupItemView.setGestaoEmpresaTreeViewModel(empresas[i]);
+                                    groupItemView.setNivel(nivel);
+
+                                    appendGroup.addView(groupItemView, i);
+                                }
+                            }
+                        }
+                    }
+                }, null);
+            }
+
+            toggleContents();
+        }
+    };
+
+    private void toggleContents()
+    {
+        if(appendGroup.isShown())
+        {
+            slideUp(appendGroup);
+            appendGroup.setVisibility(View.GONE);
+        }
+        else{
+            slideDown(appendGroup);
+            appendGroup.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void slideDown(View v)
+    {
+        Animation a = AnimationUtils.loadAnimation(context, R.anim.slide_down);
+        if(a != null)
+        {
+            a.reset();
+            if(v != null)
+            {
+                v.clearAnimation();
+                v.startAnimation(a);
+            }
+        }
+    }
+
+    private void slideUp(View v)
+    {
+        Animation a = AnimationUtils.loadAnimation(context, R.anim.slide_up);
+        if(a != null)
+        {
+            a.reset();
+            if(v != null)
+            {
+                v.clearAnimation();
+                v.startAnimation(a);
+            }
         }
     }
 }
